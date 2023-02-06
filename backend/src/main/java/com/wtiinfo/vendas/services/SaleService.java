2ca7403cf4fdb67d71ba713aef3a1a1a59bf5034
Wando Borges <wanbrwan@gmail.com>
2023-02-06 16:36:02 -0300

    Soluction to search recursive, only to training

--- backend/src/main/java/com/wtiinfo/vendas/services/SaleService.java
+++ backend/src/main/java/com/wtiinfo/vendas/services/SaleService.java
@@ -1,13 +1,16 @@
 package com.wtiinfo.vendas.services;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.wtiinfo.vendas.dto.SaleDTO;
 import com.wtiinfo.vendas.entities.Sale;
 import com.wtiinfo.vendas.repositories.SaleRepository;
+import com.wtiinfo.vendas.repositories.SellerRepository;
 
 @Service
 public class SaleService {
@@ -15,7 +18,12 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
+	@Autowired
+	private SellerRepository sellerRepo;
+	
+	@Transactional(readOnly = true) //para garantir que não fazer loking de escrita no banco
 	public Page<SaleDTO> findAll(Pageable pageable) {
+		sellerRepo.findAll();//trazendo para o cache evitando a reconsulta no banco. solucao simples, menos indicada
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(sale -> new SaleDTO(sale));
 	}
